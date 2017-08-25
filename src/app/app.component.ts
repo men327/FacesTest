03d53cd7594daf81f821b2e4f9661ef454be958e
men327 <judres03@gmail.com>
2017-08-25 09:50:12 -0500

    Se funcionalidad de cambio de color de fondo.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,36 +8,49 @@ import { Observable } from 'rxjs/Rx';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';  
   face: any;
-
+  colorName: any = 'ffffff';
+  colorBack: any = 'ffffff';
   private Eyes: any =[];
   private Mouths: any =[];
   private Noses: any =[];
   private Colors: any =['ff5733', '44ec1a', '1abfec', '8e8895', 'ee7d7d', 'a14df0', '4d52f0', 'f0eb4d'];
+  private ColorsBack: any =['ff1733', '44f01a', '1abfec', '8e8895', '0e1a7d', 'a14df0', '4d52f0', 'f0eb4d'];
 
   constructor(private FaceService: FaceService){
+    //Se llama servicio para obtener las caracteristicas de las caras
     FaceService.getListFaces().subscribe(data => {
       this.Eyes = data.face.eyes;
       this.Mouths = data.face.mouth;
       this.Noses = data.face.nose;
     });    
 
+    //Se llama servicio para obtener lista de colores
     FaceService.getColors().subscribe(data => {
       this.Colors = data.Colors;
-      console.log(this.Colors);
     });
 
+    var size = Math.round(Math.random()*500+Math.random()*100)+10;
+    FaceService.getColorsSize(size).subscribe(data => {
+      this.ColorsBack = data.Colors;
+    });
+
+    //Función, que se actualiza cada dos segundos para obtener cara
     Observable.interval(2000).subscribe(x => {        
       var indexEye = Math.round(this.Eyes.length*Math.random());
       var indexMouth = Math.round(this.Mouths.length*Math.random());
       var indexNose = Math.round(this.Noses.length*Math.random());
       var indexColor = Math.round(this.Colors.length*Math.random());
+      var indexColorBack = Math.round(this.ColorsBack.length*Math.random());
 
+      this.colorBack = '#'+this.ColorsBack[indexColorBack];
+      this.colorName = '#'+this.Colors[indexColor];
       this.face = 'https://api.adorable.io/avatars/face/'+this.Eyes[indexEye]+'/'+this.Noses[indexNose]+'/'+this.Mouths[indexMouth]+'/'+this.Colors[indexColor];
+      
+      //Se llama servicio para obtener cara desde el API Adorable
       FaceService.getFace(this.Eyes[indexEye], this.Noses[indexNose], this.Mouths[indexMouth], this.Colors[indexColor])
         .subscribe(data => {
-        console.log(data.text());
+          data.text();
         });
     });
   }
--- src/app/face.service.ts
+++ src/app/face.service.ts
@@ -7,6 +7,7 @@ export class FaceService {
   
   constructor (private _http: Http) {}
 
+  //Se obtiene la lista de las caracteristicas de las caras
   public getListFaces() {
     let headers = new Headers({ 'Access-Control-Allow-Headers': 'Content-Type'});
     let options = new RequestOptions({ headers: headers });
@@ -14,6 +15,7 @@ export class FaceService {
     .map((res: Response) => res.json());
   }
 
+  //Obtiene un face, de acuerdo a los parametros que se tienen
   public getFace(eye, nose, mouth, color) {
     let headers = new Headers({ 'Access-Control-Allow-Headers': 'Content-Type'});
     let options = new RequestOptions({ headers: headers });
@@ -21,10 +23,19 @@ export class FaceService {
     .map((res: Response) => res);
   }
 
+  //Para obtener lista de colores que se tiene en la ApiFacesTest
   public getColors() {
     let headers = new Headers({ 'Access-Control-Allow-Headers': 'Content-Type'});
     let options = new RequestOptions({ headers: headers });
     return this._http.get('http://localhost:3000/colors')
+    .map((res: Response) => res.json());
+  }
+
+  //Para obtener lista de colores que se tiene en la ApiFacesTest con un tamaño determinado
+  public getColorsSize(size) {
+    let headers = new Headers({ 'Access-Control-Allow-Headers': 'Content-Type'});
+    let options = new RequestOptions({ headers: headers });
+    return this._http.get('http://localhost:3000/colors/'+size)
     .map((res: Response) => res.json());
   }
 
